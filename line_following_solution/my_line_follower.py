42940cbc594cb892a824059e30e07d3a1f4420dc
Victor Odaudu <odauduiko@gmail.com>
2026-05-20 10:50:31 +0200

    Update my_line_follower.py

--- line_following_solution/my_line_follower.py
+++ line_following_solution/my_line_follower.py
@@ -64,7 +64,7 @@ class MyLineFollower(LineFollowingInterface):
         self._frame_count = 0
         
         # Load trained SVM model
-        model_path = os.path.join("/app", "team5_svm_final.pkl")
+        model_path = os.path.join("../", "team5_svm_final.pkl")
         self.svm = joblib.load(model_path)
         self.get_logger().info("SVM model loaded successfully.")
         
